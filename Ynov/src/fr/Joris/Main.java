32686b1b0feea9b685c4118de59e7f2c25357b99
Joris P <71002181+Miisterone@users.noreply.github.com>
2021-09-28 16:34:44 +0200

    Add out folder

--- Ynov/src/fr/Joris/Main.java
+++ Ynov/src/fr/Joris/Main.java
@@ -7,7 +7,6 @@ public class Main {
     public static void main(String[] args) {
         Voiture A = new Voiture();
         A.Voiture("test",20);
-        A.R
     }
 
     private static void Convert(){
